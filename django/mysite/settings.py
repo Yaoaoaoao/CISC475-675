5c57e65ffe0f52a3f99a3c90ccb70264009e80c8
Jia Ren <renj@udel.edu>
2017-03-04 18:21:33 -0500

    add a test page to questionnaire app

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ["huoenter.com", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
-    'questionnaire.apps.QuestionnaireConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'questionnaire', 
 ]
 
 MIDDLEWARE = [
