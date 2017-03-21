0ab3aed65feea6ff51f60b53433617dcba892186
Jia Ren <renj@udel.edu>
2017-03-21 13:10:11 -0400

    add questionnaire title & description

--- django/questionnaire/views.py
+++ django/questionnaire/views.py
@@ -8,15 +8,19 @@ def index(request):
 
 
 def questionView(request, qid):
-    questions = Question.objects.filter(questionnaire__id=qid).order_by('question_order')
-    context = {'questionnaire': []}
+    questionnaire = Questionnaire.objects.get(id=qid)
+    questions = Question.objects.filter(questionnaire__id=qid).order_by(
+        'question_order')
+    context = {'questionnaire': questionnaire,
+               'questions': []}
     for question in questions:
         qobj = {'order': question.id,
                 'question_text': question.question_text,
                 'type': question.question_type,
-                'options': Option.objects.filter(question__id=question.id).order_by('option_order')
+                'options': Option.objects.filter(
+                    question__id=question.id).order_by('option_order')
                 }
 
-        context['questionnaire'].append(qobj)
+        context['questions'].append(qobj)
 
     return render(request, 'questionnaire/question.html', context)
