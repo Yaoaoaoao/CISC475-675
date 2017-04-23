fca333bc5ae4002622ad35f123677a9cf6f1a2f9
Jia Ren <renj@udel.edu>
2017-04-23 03:23:20 -0400

    add progress bar

--- iOS/ReturnToSports/WebViewController.swift
+++ iOS/ReturnToSports/WebViewController.swift
@@ -13,7 +13,7 @@ class WebViewController: UIViewController, WKNavigationDelegate {
     // MARK: Property
     var webView: WKWebView!
     var progressView: UIProgressView!
-    
+
     let baseUrl = "http://127.0.0.1:8000/questionnaire"
     
     override func loadView() {
@@ -29,14 +29,29 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
         
+        progressView = UIProgressView(frame: CGRect(x: 0, y: 64, width: UIScreen.main.bounds.width, height: 75))
+        webView.addSubview(progressView)
+        progressView.isHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     }
+    
+    deinit { webView.removeObserver(self, forKeyPath: "estimatedProgress") }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
     }
     
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+            if self.webView.estimatedProgress >= 1.0 {
+                progressView.isHidden = true
+            }
+        }
+    }
     
-    override func didReceiveMemoryWarning() {
+     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
