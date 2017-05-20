5e7c4c35543b765f11ffb73f790436ca83d92466
Jia Ren <renj@udel.edu>
2017-05-19 20:47:27 -0400

    add an index page for doctors

--- django/response/urls.py
+++ django/response/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-
+from django.views.generic import TemplateView
 from views import *
 
 urlpatterns = [
-    url(r'view/$', responseTable, name='response'),
+    url(r'^$', TemplateView.as_view(template_name='response/index.html'), name='index'),
+    url(r'view/$', responseTable, name='view'),
     url(r'track/$', trackView, name='track')
 ]
